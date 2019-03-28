6d13a00470b0119880e004f5b07b1f43824b9f69
jvalero-Praxya <jvalero@praxya.com>
2019-03-28 17:03:44 +0100

    Añadido nuevo módulo/falla instalación

---
+++ product_extend/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Product",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Libreria
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/o
+    # doo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Product',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['product'],
+
+    # always loaded
+    'data': [
+        'views/product_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+
+}
---
+++ product_extend/models/product_template.py
@@ -0,0 +1,14 @@
+ #-*- coding: utf-8 -*-
+
+from odoo import api, models, fields
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    tipology = fields.Selection(
+        selection=[
+            ['metal', 'Metal']
+            ['pwc', 'PWC']
+            ['cristal', 'Cristal']
+        ]
+    )
