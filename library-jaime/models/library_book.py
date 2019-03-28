d0b5c03fadc2a645008f8605ead1f8fb1d4482c0
jvalero-Praxya <jvalero@praxya.com>
2019-03-28 14:02:20 +0100

    Vistas terminadas

--- library-jaime/models/library_book.py
+++ library-jaime/models/library_book.py
@@ -1,14 +1,40 @@
-# -*- coding: utf-8 -*-
+ #-*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import api, models, fields, exceptions
 
 
 class LibraryBook(models.Model):
-    _name="library.book"
+    _name = "library.book"
 
     name= fields.Char(string="Book")
-    description= fields.Text(string="Description")
+
+    description = fields.Text(string="Description")
+
+    isbn = fields.Char("ISBN")
+
 
     category_id = fields.Many2one(
 
-        comodel_name="library.category", string="Categoria")
+        comodel_name="library.category",
+        inverse_name="book_id",
+        string="Categorias")
+
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)', 'El nombre del libro no puede repetirse',)
+    ]
+
+    categ_count = fields.Integer(
+        string="Nº de Categorias",
+        compute="_count_categ"
+    )
+
+    def _count_categ(self):
+        for book in self:
+            book.categ_count = len(book.category_id)
+
+ #@api.constrains("isbn")
+    #    def check_isbn(self):
+    #    isbn = self.search([['id', '!=', self.id]]).mapped("isbn")
+    #    if self.isbn and self.isbn in isbn:
+ #        raise.exceptions.ValidationError("ISBN Duplicado")
+    #Descomentar y revisar
